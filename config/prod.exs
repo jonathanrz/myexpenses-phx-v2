aa017560af48678a21b41c73528f0a14100f4fff
Jonathan Zanella <jonathanrafaelz@gmail.com>
2019-09-21 16:51:27 -0300

    add env var for host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :myexpenses_phx_v2, MyexpensesPhxV2Web.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "myexpenses-phx-v2-stg", port: 443],
+  url: [scheme: "https", host: System.get_env("APP_HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
