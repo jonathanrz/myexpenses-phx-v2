bfdc92e0ba12386d5027bcea62cce90b08a9aedf
Jonathan Zanella <jonathanrafaelz@gmail.com>
2019-09-29 18:45:58 -0300

    form style

--- lib/myexpenses_phx_v2_web/plugs/set_title.ex
+++ lib/myexpenses_phx_v2_web/plugs/set_title.ex
@@ -7,6 +7,8 @@ defmodule MyexpensesPhxV2Web.Plugs.SetTitle do
 
   def call(conn, _params) do
     case conn.path_info do
+      ["accounts", "new"] -> assign(conn, :title, "New Account")
+      ["accounts", _id, "edit"] -> assign(conn, :title, "Edit Account")
       ["accounts" | _tail] -> assign(conn, :title, "Accounts")
       _ -> assign(conn, :title, "My Expenses")
     end
