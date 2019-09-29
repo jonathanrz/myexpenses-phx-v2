ececdf01e53703706f162dcd7d6dd3c8745f5457
Jonathan Zanella <jonathanrafaelz@gmail.com>
2019-09-29 19:18:02 -0300

    show page

--- lib/myexpenses_phx_v2_web/plugs/set_title.ex
+++ lib/myexpenses_phx_v2_web/plugs/set_title.ex
@@ -9,6 +9,7 @@ defmodule MyexpensesPhxV2Web.Plugs.SetTitle do
     case conn.path_info do
       ["accounts", "new"] -> assign(conn, :title, "New Account")
       ["accounts", _id, "edit"] -> assign(conn, :title, "Edit Account")
+      ["accounts", _id] -> assign(conn, :title, "Account")
       ["accounts" | _tail] -> assign(conn, :title, "Accounts")
       _ -> assign(conn, :title, "My Expenses")
     end
