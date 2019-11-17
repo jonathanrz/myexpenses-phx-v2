e2663a5d39069505a7ec3ab3f4d7cd4ebf8152fe
Jonathan Zanella <jonathanrafaelz@gmail.com>
2019-11-17 15:03:23 -0300

    category model

--- test/myexpenses_phx_v2/data_test.exs
+++ test/myexpenses_phx_v2/data_test.exs
@@ -288,4 +288,71 @@ defmodule MyexpensesPhxV2.DataTest do
       assert %Ecto.Changeset{} = Data.change_place(place)
     end
   end
+
+  describe "categories" do
+    alias MyexpensesPhxV2.Data.Category
+
+    @valid_attrs %{name: "some name"}
+    @update_attrs %{name: "some updated name"}
+    @invalid_attrs %{name: nil}
+
+    def category_fixture(attrs \\ %{}) do
+      {:ok, category} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Data.create_category()
+
+      category
+    end
+
+    @tag :skip
+    test "list_categories/0 returns all categories" do
+      category = category_fixture()
+      assert Data.list_categories() == [category]
+    end
+
+    @tag :skip
+    test "get_category!/1 returns the category with given id" do
+      category = category_fixture()
+      assert Data.get_category!(category.id) == category
+    end
+
+    @tag :skip
+    test "create_category/1 with valid data creates a category" do
+      assert {:ok, %Category{} = category} = Data.create_category(@valid_attrs)
+      assert category.name == "some name"
+    end
+
+    @tag :skip
+    test "create_category/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Data.create_category(@invalid_attrs)
+    end
+
+    @tag :skip
+    test "update_category/2 with valid data updates the category" do
+      category = category_fixture()
+      assert {:ok, %Category{} = category} = Data.update_category(category, @update_attrs)
+      assert category.name == "some updated name"
+    end
+
+    @tag :skip
+    test "update_category/2 with invalid data returns error changeset" do
+      category = category_fixture()
+      assert {:error, %Ecto.Changeset{}} = Data.update_category(category, @invalid_attrs)
+      assert category == Data.get_category!(category.id)
+    end
+
+    @tag :skip
+    test "delete_category/1 deletes the category" do
+      category = category_fixture()
+      assert {:ok, %Category{}} = Data.delete_category(category)
+      assert_raise Ecto.NoResultsError, fn -> Data.get_category!(category.id) end
+    end
+
+    @tag :skip
+    test "change_category/1 returns a category changeset" do
+      category = category_fixture()
+      assert %Ecto.Changeset{} = Data.change_category(category)
+    end
+  end
 end
