b47ab4f9881960024568aa05722bfa9a8bc88ffc
Jonathan Zanella <jonathanrafaelz@gmail.com>
2020-03-22 15:48:30 -0300

    delete

--- assets/js/components/Accounts/Table/index.js
+++ assets/js/components/Accounts/Table/index.js
@@ -6,13 +6,16 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import Link from "@material-ui/core/Link";
 
 import ThemeProvider from "../../shared/ThemeProvider";
+import getCSRFToken from "../../../helpers/getCSRFToken";
+
+const csrfToken = getCSRFToken();
 
 function AccountsTable({ accounts }) {
-  console.log({ accounts });
   return (
     <ThemeProvider>
       <TableContainer component={Paper}>
@@ -34,6 +37,16 @@ function AccountsTable({ accounts }) {
                 <TableCell align="center">
                   <Link href={`accounts/${account.id}/edit`}>
                     <EditIcon />
+                  </Link>
+                  <Link
+                    href={`accounts/${account.id}`}
+                    data-confirm="Are you sure?"
+                    data-method="delete"
+                    data-to={`/accounts/${account.id}`}
+                    data-csrf={csrfToken}
+                    rel="nofollow"
+                  >
+                    <DeleteIcon />
                   </Link>
                 </TableCell>
               </TableRow>
