e6fc8372ca000f8dfbb9ec4a6dde4da44f79a986
Jonathan Zanella <jonathanrafaelz@gmail.com>
2020-05-18 20:52:40 -0300

    clean up

--- assets/js/components/Accounts/List.js
+++ assets/js/components/Accounts/List.js
@@ -1,14 +1,8 @@
 import React from "react";
-
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import AddIcon from "@material-ui/icons/Add";
-import Link from "@material-ui/core/Link";
 
 import Card from "./Card";
 import Form from "./Form";
-
-const PATH = "accounts";
 
 function AccountList({ data }) {
   return (
