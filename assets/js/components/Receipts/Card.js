6fa5c8a1cae0ecc49c641a5b78b25fda89dd8ae7
Jonathan Zanella <jonathanrafaelz@gmail.com>
2020-05-26 20:49:55 -0300

    confirm/unconfirm button

--- assets/js/components/Receipts/Card.js
+++ assets/js/components/Receipts/Card.js
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import moment from "moment";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Typography from "@material-ui/core/Typography";
 import CardModel from "../shared/CardModel";
 import Currency from "../../helpers/currency";
+import getCSRFToken from "../../helpers/getCSRFToken";
 import Form from "./Form";
 
 const PATH = "receipts";
 
+const csrfToken = getCSRFToken();
+
 function ReceiptCard({ data, accounts }) {
+  function confirmLink() {
+    return `/${PATH}/${data.id}/${data.confirmed ? "unconfirm" : "confirm"}`;
+  }
+
+  const confirmButtonProps = {
+    href: confirmLink(),
+    "data-confirm": "Are you sure?",
+    "data-method": "post",
+    "data-to": confirmLink(),
+    "data-csrf": csrfToken,
+    rel: "nofollow",
+  };
+
   return (
     <CardModel data={data} path={PATH} Form={Form} formParams={{ accounts }}>
       <Typography
@@ -24,9 +42,32 @@ function ReceiptCard({ data, accounts }) {
       <Typography variant="body1" color="textSecondary" component="p">
         <b>Value:</b> {Currency.format(data.value)}
       </Typography>
-      <Typography variant="body1" color="textSecondary" component="p">
+      <Typography
+        variant="body1"
+        color="textSecondary"
+        component="p"
+        gutterBottom
+      >
         <b>Account:</b> {data.account.name}
       </Typography>
+      <ButtonGroup
+        size="small"
+        color="primary"
+        aria-label="small outlined button group"
+      >
+        <Button
+          variant={data.confirmed ? "contained" : "outlined"}
+          {...confirmButtonProps}
+        >
+          {data.confirmed ? "Confirmed" : "Confirm"}
+        </Button>
+        <Button
+          variant={data.confirmed ? "outlined" : "contained"}
+          {...confirmButtonProps}
+        >
+          {data.confirmed ? "Unconfirm" : "Unconfirmed"}
+        </Button>
+      </ButtonGroup>
     </CardModel>
   );
 }
